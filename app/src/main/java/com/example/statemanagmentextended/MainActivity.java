55cad53f18db34424ea5237a666851da45b216c1
PolskiZiemniak <polskiziemniak2018@gmail.com>
2024-10-03 14:04:17 +0200

    Finished save instatnce state

--- app/src/main/java/com/example/statemanagmentextended/MainActivity.java
+++ app/src/main/java/com/example/statemanagmentextended/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.statemanagmentextended;
 
-import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,23 +11,27 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String KEY_COUNTER = "counter";
     private static final String KEY_USERTEXT = "userText";
     private static final String KEY_CHECKBOX = "checkBox";
-    private static final String KEY_CHANGEMODE = "changeMode";
+    private static final String KEY_SWITCH = "changeMode";
 
     private TextView showNumber;
     private Button increaseNumber;
     private EditText userText;
     private CheckBox checkBox;
-    private Switch changeMode = findViewById(R.id.changeMode);
+    private TextView hiddenText;
+    private Switch aSwitch;
+    private ConstraintLayout background;
+
     private int counter = 0;
+    private String text = "";
+    private Boolean isChecked = false;
+    private Boolean isSwitched = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +43,20 @@ public class MainActivity extends AppCompatActivity {
         increaseNumber = findViewById(R.id.increaseNumber);
         userText = findViewById(R.id.userText);
         checkBox = findViewById(R.id.checkBox);
+        hiddenText = findViewById(R.id.hiddenText);
+        aSwitch = findViewById(R.id.changeMode);
+        background = findViewById(R.id.main);
 
         if(savedInstanceState != null){
             counter = savedInstanceState.getInt(KEY_COUNTER);
-            userText.setText(savedInstanceState.getString(KEY_USERTEXT));
-            checkBox.setChecked(savedInstanceState.getBoolean(KEY_CHECKBOX));
-
+            text = savedInstanceState.getString(KEY_USERTEXT);
+            isChecked = savedInstanceState.getBoolean(KEY_CHECKBOX);
+            isSwitched = savedInstanceState.getBoolean(KEY_SWITCH);
         }
         updateCounter();
-        updateMode();
+        updateUserText();
+        updateCheckbox();
+        updateSwitch();
 
         increaseNumber.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,21 +65,51 @@ public class MainActivity extends AppCompatActivity {
                 updateCounter();
             }
         });
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isChecked = checkBox.isChecked();
+                updateCheckbox();
+            }
+        });
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSwitched = aSwitch.isChecked();
+                updateSwitch();
+            }
+        });
     }
     @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
         outState.putInt(KEY_COUNTER, counter);
+        outState.putString(KEY_USERTEXT, text);
+        outState.putBoolean(KEY_CHECKBOX, isChecked);
+        outState.putBoolean(KEY_SWITCH, isSwitched);
     }
     private void updateCounter(){
         showNumber.setText("Ilość kliknięć: "+counter);
     }
-    private void updateMode(){
-        View view = this.getWindow().getDecorView();
-        if(!changeMode.isChecked()){
-            view.setBackgroundColor(1);
-        }else{
-            view.setBackgroundColor(2);
+    private void updateUserText(){
+        userText.setText(text);
+    }
+    private void updateCheckbox(){
+        if(isChecked){
+            hiddenText.setVisibility(View.VISIBLE);
+            checkBox.setChecked(true);
+        }else {
+            hiddenText.setVisibility(View.INVISIBLE);
+            checkBox.setChecked(false);
+        }
+    }
+    private void updateSwitch(){
+        if (isSwitched) {
+            background.setBackgroundColor(Color.BLACK);
+            aSwitch.setChecked(true);
+        }else {
+            background.setBackgroundColor(Color.WHITE);
+            aSwitch.setChecked(false);
         }
     }
 }
